8af1ee2498dbe204da09b44b251b9337b31cd41f
Fabuss254 <26258237+fabuss254@users.noreply.github.com>
2022-11-30 20:27:14 +0100

    e

--- farm.lua
+++ farm.lua
@@ -1,1 +1,16 @@
-print(turtle.getFuelLevel())
+local Data = {}
+function Setup()
+    turtle.left()
+    turtle.left()
+    print("Block in front: ", turtle.inspect())
+end
+
+function CheckForRefuel()
+    if turtle.getFuelLevel() <= 1 then
+        turtle.refuel(1)
+        print("Fuel level:", turtle.getFuelLevel())
+    end
+end
+
+CheckForRefuel()
+Setup()
